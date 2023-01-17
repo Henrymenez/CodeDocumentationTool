060cfb7f16e89eb15367eb14a72ae9145242830b
Henry Ugochukwu <henryugochukwuagu@gmail.com>
2023-01-17 21:59:26 +0100

    created a model and a custom atttribute

--- ConsoleTester/Program.cs
+++ ConsoleTester/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace ConsoleTester
+﻿using DocumentationAttribute.Data;
+namespace ConsoleTester
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Person.Display();
         }
     }
 }
