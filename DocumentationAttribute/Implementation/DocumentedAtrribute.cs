a363ca6179c5252ea6d4b264ac35c7d1e8432b14
Henry Ugochukwu <henryugochukwuagu@gmail.com>
2023-01-20 09:56:21 +0100

    .

--- DocumentationAttribute/Implementation/DocumentedAtrribute.cs
+++ DocumentationAttribute/Implementation/DocumentedAtrribute.cs
@@ -15,12 +15,14 @@ namespace DocumentationAttribute.Implementation
                 var types = assembly.GetTypes();
                 foreach (var type in types)
                 {
+                    
                     var members = type.GetMembers();
                     foreach (var member in members)
                     {
                         var attributes = member.GetCustomAttributes(typeof(DescriptionAttribute), false);
                         if (attributes.Length > 0)
                         {
+                            Console.WriteLine("Type name: " + type.Name);
                             Console.WriteLine("Member Type: {0}", member.MemberType);
                             Console.WriteLine("Name: {0}", member.Name);
                             Console.WriteLine($"Description: {((DescriptionAttribute)attributes[0]).Description}");
